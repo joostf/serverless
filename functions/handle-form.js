a4acefd849b80086b04d8c72e1b144ead5e60f98
joostf <j.faber@hva.nl>
2023-02-01 22:37:00 +0100

    tweaks

--- functions/handle-form.js
+++ functions/handle-form.js
@@ -1,31 +1,29 @@
-// // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-// const handler = async (event) => {
-//   try {
-//     const subject = event.queryStringParameters.name || 'World'
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify({ message: `Hello ${subject}` }),
-//       // // more keys you can return:
-//       // headers: { "headerName": "headerValue", ... },
-//       // isBase64Encoded: true,
-//     }
-//   } catch (error) {
-//     return { statusCode: 500, body: error.toString() }
-//   }
-// }
+// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+const handler = async (event) => {
+  console.log(event.body)
+  try {
+    const data = JSON.parse(event.body);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Form submitted successfully!", data:data }),
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
 
-// module.exports = { handler }
+module.exports = { handler }
 
 
-exports.handler = async (event, context) => {
-  const data = JSON.parse(event.body);
-  // Perform logic with the form data, such as sending an email, storing data in a database, etc.
+// exports.handler = async (event, context) => {
+//   const data = JSON.parse(event.body);
+//   // Perform logic with the form data, such as sending an email, storing data in a database, etc.
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Form submitted successfully!",
-      data: data
-    })
-  };
-};
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       message: "Form submitted successfully!",
+//       data: data
+//     })
+//   };
+// };
