5e71dfcdc797467fc4981cd4b1142a5421065280
joostf <j.faber@hva.nl>
2023-02-02 10:06:24 +0100

    added formfields && post to hygrah api

--- functions/handle-form.js
+++ functions/handle-form.js
@@ -3,27 +3,53 @@ const handler = async (event) => {
   console.log(event.body)
   try {
     const data = JSON.parse(event.body);
+
+    const mutatedData = await postMemberData(data)
+    console.log(mutatedData)
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Form submitted successfully!", data:data }),
+      body: JSON.stringify({ message: `Member: ${mutatedData.name} updated!`, data:mutatedData }),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
   }
 }
 
+
+async function postMemberData (data) {
+  const token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE2NzUwNjg4NzgsImF1ZCI6WyJodHRwczovL2FwaS1ldS1jZW50cmFsLTEtc2hhcmVkLWV1YzEtMDIuaHlncmFwaC5jb20vdjIvY2xkMXMwMmhsMGFhYTAxdGJleHo2MzJwaC9tYXN0ZXIiLCJtYW5hZ2VtZW50LW5leHQuZ3JhcGhjbXMuY29tIl0sImlzcyI6Imh0dHBzOi8vbWFuYWdlbWVudC5ncmFwaGNtcy5jb20vIiwic3ViIjoiNThiYzk0M2EtNjZlZS00YmNmLTliNDYtNWIzNDAxZTFhMzIyIiwianRpIjoiY2xkaWtyZW9nMjRvZTAxdGUzdWUzMnY5aiJ9.2edEX6Si5Xj6DF6NldPq3SOLi7R46cjDF8E0RjK4sm2mOijccqLwOPHzRAparCR_M1CdrRTTJgD_VsqiTs74ihV1wo7nTXrkngToFy4AiqkErcI58NOukfgscfCjK9FTQ874KFmv35jYC6nBxp3tyJluFgL5BTKNTcoHKffVRUYPxOtt2w1I8REcgv742T3bpyf0K4eDmUkVjGwFIK913z5OPQ7AWD6leRDWHQfzzJX6fDaEYEzc_7rztIXDqifvD00M1H_keNgkbQOl56bMCALFQC0A72BJ7DIBId47O9EJJUBiGYzP5CHYGOrgQNLQGJ13b_LUr8tbR8PTMiizz51MPYNQ5YHIOT97QVzES1VCXdbDpza8vALX-BTU8oiL0UVHq31iFL7FnQ_iCIw99qRJqCLYR11zcZ1RlMLGy57sOzPXA6bGiIDMkGD2d-2JEqMrZHmg5EufBTVoGHVRtZPSO7JUzMEn6LIW-KWhlrwJKyXcd6POTEf3ah4nmhYGVkXF2JX-9iNmwMwJnPfD6E921IEgBXVv5-Y9LdSvcQznWZnO1e0xz5extWf6wPK6gEwobaoXDcGUFRx9SF3Bp8CKA0X4BthVHMwY3KVJr5t7W09JoTpIVyEool7tD7zP13QAWop7DFjd8SMnVXaKXDpJuOfOnfBaCurlGao_NFI'
+  const query = `
+  mutation {
+      updateMember(
+          where: {id: "cldcsnrpc0z9w0auooju1uj1g"}
+          data: {nickname:"Faab"}
+      ) {
+          id
+          name
+          prefix
+          surname
+          nickname
+          avatar
+          bio {
+              html
+          }
+          website
+          role
+      }
+  }
+  `
+
+  const variables = {} 
+  const headers = { Authorization: `Bearer ${token}` }
+  const response = await fetch(endpoint, {
+      method: 'POST',
+      body: JSON.stringify({ query, variables }), 
+      headers: headers
+  })
+  const tribeData = await response.json()
+
+  console.dir(tribeData)
+}
+
 module.exports = { handler }
-
-
-// exports.handler = async (event, context) => {
-//   const data = JSON.parse(event.body);
-//   // Perform logic with the form data, such as sending an email, storing data in a database, etc.
-
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify({
-//       message: "Form submitted successfully!",
-//       data: data
-//     })
-//   };
-// };
