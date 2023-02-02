81b26bdda6044b896d08d3a6fc70d61afc27a442
joostf <j.faber@hva.nl>
2023-02-02 10:34:08 +0100

    Update handle-form.js

--- functions/handle-form.js
+++ functions/handle-form.js
@@ -1,6 +1,7 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
-  //try {
+  console.log(1, event.body)
+  try {
     const data = JSON.parse(event.body);
 
     //const mutatedData = await postMemberData(data)
@@ -10,9 +11,9 @@ const handler = async (event) => {
       statusCode: 200,
       body: JSON.stringify({ message: `Member updated!`, data:data }),
     }
-  // } catch (error) {
-  //   return { statusCode: 500, body: error.toString() }
-  // }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
 }
 
 
